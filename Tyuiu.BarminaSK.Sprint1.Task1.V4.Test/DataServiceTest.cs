a1ce4cd1fb68b143b4471b45e935af0fd9281ed2
BarminaSK <barminask@mail.ru>
2025-09-30 22:27:46 +0500

    Task1

---
+++ Tyuiu.BarminaSK.Sprint1.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BarminaSK.Sprint1.Task1.V4.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task1.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(6, res);
+
+        }
+    }
+}

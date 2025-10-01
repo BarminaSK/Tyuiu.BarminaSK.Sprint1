483407c2ff0e7d7224e07776d6e08037b1c8ce75
BarminaSK <barminask@mail.ru>
2025-10-01 08:18:49 +0500

    Task3

---
+++ Tyuiu.BarminaSK.Sprint1.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BarminaSK.Sprint1.Task3.V8.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task3.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double startAmount = 2500.0;
+            double percent = 20.0;
+            double timeDays = 30.0;
+            double wait = 41.096;
+            var res = ds.IncomeAmount(startAmount, percent, timeDays);
+            Assert.AreEqual(wait, res,0.001);
+        }
+    }
+}
---
+++ Tyuiu.BarminaSK.Sprint1.Task3.V8/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BarminaSK.Sprint1.Task3.V8.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task3.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Бармина С.К. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнила: Бармина София Константиновна | ПИНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x = 2500.0;
+            double y = 20.0;
+            double z = 30.0;
+            Console.WriteLine("Величина вклада = " + x);
+            Console.WriteLine("Процентная ставка = " + y);
+            Console.WriteLine("Срок вклада = " + z);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.IncomeAmount(x, y, z));
+
+            Console.ReadKey();
+
+        }
+    }
+}

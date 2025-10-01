eedc865b9615e79520706568ba1c4abab2d27e96
BarminaSK <barminask@mail.ru>
2025-10-01 08:52:04 +0500

    Task4

---
+++ Tyuiu.BarminaSK.Sprint1.Task4.V21.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task4.V21.Lib
+{
+    public class DataService : ISprint1Task4V21
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = Math.Round(Math.Abs(Math.Pow((1 + x), 2) - y) / (x + y), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BarminaSK.Sprint1.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BarminaSK.Sprint1.Task4.V21.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 0.667;
+            var res = ds.Calculate(x,y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

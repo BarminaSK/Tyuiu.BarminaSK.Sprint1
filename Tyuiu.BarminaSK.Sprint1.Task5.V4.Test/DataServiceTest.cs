6b6a164bff4235a0ee66162f99c57f776490db24
BarminaSK <barminask@mail.ru>
2025-10-01 09:21:21 +0500

    Task5

---
+++ Tyuiu.BarminaSK.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BarminaSK.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int time = 13257;
+            int wait = 3;
+            var res = ds.SecondsToHours(time);
+            Assert.AreEqual(wait, res); 
+
+        }
+    }
+}
--- Tyuiu.BarminaSK.Sprint1.Task5.V4/Program.cs
+++ Tyuiu.BarminaSK.Sprint1.Task5.V4/Program.cs
@@ -1,6 +1,6 @@
-﻿using Tyuiu.BarminaSK.Sprint1.Task4.V21.Lib;
+﻿using Tyuiu.BarminaSK.Sprint1.Task5.V4.Lib;
 
-namespace Tyuiu.BarminaSK.Sprint1.Task4.V21
+namespace Tyuiu.BarminaSK.Sprint1.Task5.V21
 {
     class Program
     {
@@ -11,32 +11,30 @@ namespace Tyuiu.BarminaSK.Sprint1.Task4.V21
             Console.Title = "Спринт #1 | Выполнила: Бармина С.К. | ПИНб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Class Math                                                        *");
-            Console.WriteLine("* Задание #4                                                              *");
-            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
             Console.WriteLine("* Выполнила: Бармина София Константиновна | ПИНб-25-1                     *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
-            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
-            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-
-            int x, y;
-            Console.WriteLine("Введите значение х:");
-            x = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Введите значение y:");
-            y = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.WriteLine("Введите время в секундах (k-я секунда суток):");
+            int x = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("|(1 + x)^ 2 - y| / x + y = " + ds.Calculate(x,y));
+
+
+            int res = ds.SecondsToHours(x);
+            Console.WriteLine(res);
 
             Console.ReadKey();
+
         }
     }
 }

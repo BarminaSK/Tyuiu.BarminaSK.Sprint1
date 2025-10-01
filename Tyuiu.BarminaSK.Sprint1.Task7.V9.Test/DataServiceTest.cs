90f4ff9f95f90e3ba202e3ec3f4e615d48381415
BarminaSK <barminask@mail.ru>
2025-10-01 12:42:27 +0500

    Task7

---
+++ Tyuiu.BarminaSK.Sprint1.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BarminaSK.Sprint1.Task7.V9.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task7.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 1.975;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BarminaSK.Sprint1.Task7.V9/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BarminaSK.Sprint1.Task7.V9.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint1.Task6.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Бармина С.К. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнила: Бармина София Константиновна | ПИНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по исход *");
+            Console.WriteLine("* значениям данных, вводимых пользователем                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.Calculate(x, y);
+            Console.WriteLine($"Z = {result}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
